e93f2b819333402cbcc6768709bb89dda7c44b0d
hidromehanika <53266138+hidromehanika@users.noreply.github.com>
2019-08-28 22:08:45 +0200

    Update ss.cs

--- ss.cs
+++ ss.cs
@@ -7,7 +7,7 @@ public class Program
 	public static void Main()
 	{
         FirstLoading();
-    }
+  	}
     enum DropDownListColors {Yellow=1, Black, Gray};
 
     public static void FirstLoading() {
@@ -22,11 +22,11 @@ public class Program
     //sender and e are default arguments
     public void DropDownList1_SelectedIndexChanged(Object sender, EventArgs e) {
         if(HttpContext.Current.Request.Cookies["boja"] == null) {
-            System.Drawing.Color selectedColor = Enum.GetName(typeof(DropDownListColors), DropDownList1.SelectedItem.Value));
-			HttpCookie colorCookie = new HttpCookie("boja");
-			colorCookie.Value = selectedColor.ToString();
-            HttpContext.Current.Response.Cookies.Add(colorCookie);
+   		System.Drawing.Color selectedColor = Enum.GetName(typeof(DropDownListColors), DropDownList1.SelectedItem.Value));
+		HttpCookie colorCookie = new HttpCookie("boja");
+		colorCookie.Value = selectedColor.ToString();
+		HttpContext.Current.Response.Cookies.Add(colorCookie);
         } else 
-            HttpContext.Current.Response.Cookies["boja"].Value= DropDownList1.SelectedItem.Value;
+            	HttpContext.Current.Response.Cookies["boja"].Value= DropDownList1.SelectedItem.Value;
     }
 }
