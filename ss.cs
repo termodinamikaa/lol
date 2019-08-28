c871e86449302cd5a8a846cd686cf339b20154b5
hidromehanika <53266138+hidromehanika@users.noreply.github.com>
2019-08-28 22:10:02 +0200

    Update ss.cs

--- ss.cs
+++ ss.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Web;
-using System.Drawing;
 
 public class Program
 {
@@ -8,13 +7,14 @@ public class Program
 	{
         FirstLoading();
   	}
+	
     enum DropDownListColors {Yellow=1, Black, Gray};
 
     public static void FirstLoading() {
         if (HttpContext.Current.Request.Cookies["firstLoading"].ToString() == null) {
 	    	HttpContext.Current.Response.Cookies["firstLoading"].Value = "is not first anymore";
                 if(HttpContext.Current.Request.Cookies["zapamti"] != null) {
-                    Label1.BackColor  = Color.FromName(HttpContext.Current.Request.Cookies["boja"].ToString());
+                    Label1.BackColor  = Drawing.Color.FromName(HttpContext.Current.Request.Cookies["boja"].ToString());
                 }
         }
     }
@@ -22,7 +22,7 @@ public class Program
     //sender and e are default arguments
     public void DropDownList1_SelectedIndexChanged(Object sender, EventArgs e) {
         if(HttpContext.Current.Request.Cookies["boja"] == null) {
-   		System.Drawing.Color selectedColor = Enum.GetName(typeof(DropDownListColors), DropDownList1.SelectedItem.Value));
+   		Drawing.Color selectedColor = Enum.GetName(typeof(DropDownListColors), DropDownList1.SelectedItem.Value));
 		HttpCookie colorCookie = new HttpCookie("boja");
 		colorCookie.Value = selectedColor.ToString();
 		HttpContext.Current.Response.Cookies.Add(colorCookie);
